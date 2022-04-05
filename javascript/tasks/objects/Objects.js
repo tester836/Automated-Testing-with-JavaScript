ebb3f6620633287080ad013e57bdfebad4e9f880
anton.pashutkin <anton.pashutkin@gxbventures.com>
2022-04-05 15:18:42 +0300

    objects and functions

--- javascript/tasks/objects/Objects.js
+++ javascript/tasks/objects/Objects.js
@@ -8,7 +8,28 @@ alert(car.color)
 
 car.power = "180 HP"
 function power() {
-    console.log(car.power)
+    console.info("Car power:", car.power)
 }
 power();
+
+
+function sumOfFruits(apple, pear) {
+   return apple + pear;
+}
+let sum = sumOfFruits(35, 44);
+alert (sum);
+
+
+function greetings() {
+    if (inputName == savedName) {
+        alert("Greetings, " + inputName + "!")
+    } else {
+        alert("No such name.")
+    }
+}
+
+let savedName = "tester";
+let inputName = prompt("What is your name?");
+greetings(inputName);
+
 
