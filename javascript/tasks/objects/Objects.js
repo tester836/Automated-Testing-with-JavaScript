47e2d1d4b4e0bf1ac0dc76ad5807a99145573981
anton.pashutkin <anton.pashutkin@gxbventures.com>
2022-04-05 13:39:29 +0300

    added tasks

---
+++ javascript/tasks/objects/Objects.js
@@ -0,0 +1,14 @@
+let car = {
+    color: "black",
+}
+alert(car.color)
+
+car.color = "green";
+alert(car.color)
+
+car.power = "180 HP"
+function power() {
+    console.log(car.power)
+}
+power();
+
