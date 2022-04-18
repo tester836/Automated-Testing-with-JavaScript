65e6782fff162f8276597eaa8207a34ed97fbfb7
anton.pashutkin <anton.pashutkin@gxbventures.com>
2022-04-18 15:59:26 +0300

    WIP arrays tasks

---
+++ tasks/arrays/Arrays.js
@@ -0,0 +1,12 @@
+let movies = [
+    'movie1',
+    'movie2',
+    'movie3'
+];
+
+$.each(movies, function (index, value){
+    console.log(index + ': ' + value);
+});
+
+let cars = [1, 2, 3, 4];
+alert(a.join('-'));
