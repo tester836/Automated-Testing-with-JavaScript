640915f7ceb48e6b313cc181a36bcbd9e9549e50
anton.pashutkin <anton.pashutkin@gxbventures.com>
2022-04-18 17:34:15 +0300

    arrays 7th task added

--- tasks/arrays/Arrays.js
+++ tasks/arrays/Arrays.js
@@ -3,10 +3,36 @@ let movies = [
     'movie2',
     'movie3'
 ];
-
 $.each(movies, function (index, value){
     console.log(index + ': ' + value);
 });
 
-let cars = [1, 2, 3, 4];
-alert(a.join('-'));
+
+let carsArray = ['vaz', 'bmw', 'kamaz'];
+let carsToString = carsArray.join('+');
+console.log(carsToString);
+let carsToArray = carsToString.split('+');
+console.log(carsToArray);
+
+
+let friends = ['John', 'Ivan', 'Bill'];
+let helloFriends = friends.map((name) => 'hello ' + name);
+console.log(helloFriends);
+
+
+const array = [1,6,7,8,3,4,5,6];
+console.log(Boolean(array));
+
+
+let descSortedArray = array.sort((a, b) => b - a);
+console.log(descSortedArray); 
+
+
+let filteredArray = array.filter(num => num > 3);
+console.log(filteredArray);
+
+
+function indexOfNumber(arr, num) {
+    return arr.indexOf(num);
+}
+console.log(indexOfNumber([1,6,7,8,3,4,5,6], 3));
