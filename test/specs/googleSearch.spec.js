7b09d9f9d2f50050deec22b6731edf800a4c92ff
tester836 <tester836@ya.ru>
2022-04-26 17:06:58 +0300

    ui tests WIP

--- test/specs/googleSearch.spec.js
+++ test/specs/googleSearch.spec.js
@@ -1,12 +1,25 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
-(async function example() {
-  let driver = await new Builder().forBrowser('firefox').build();
+(async function firstScript() {
   try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-  } finally {
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    await driver.get('https://www.google.com');
+    await driver.getTitle();
+
+    driver.manage().setTimeouts({implicit: 1000})
+    driver.manage().window().maximize();
+
+    let searchBox = await driver.findElement(By.name('q'));
+    let searchButton = await driver.findElement(By.name('btnK'));
+
+    await searchBox.sendKeys('Selenium');
+    await searchButton.click();
+
+    await driver.findElement(By.name('q')).getAttribute("value");
+
     await driver.quit();
+  } catch (error) {
+    console.log(error)
   }
 })();
